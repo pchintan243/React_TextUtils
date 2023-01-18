4eb321bd386c8228f4a59a78eb38aa3dfcd7c63b
Chintan Patel <patelchintan843@gmail.com>
2023-01-18 18:26:10 +0530

    v13 complete

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import React, { useState } from "react";
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import Alert from "./components/Alert";
 
 function App() {
 
   // Check Whether the dark mode is enabled or not
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
 
   const toggleMode = () => {
     if (mode === "dark") {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      // showAlert("Light mode has been enabled", "success");
+      showAlert("Light mode has been enabled", "success");
     }
     else {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-      // showAlert("Dark mode has been enabled", "success");
+      showAlert("Dark mode has been enabled", "success");
     }
   };
 
@@ -31,7 +43,9 @@ function App() {
       {/* For default props */}
       {/* <Navbar /> */}
 
-      <TextForm heading="Address" mode={mode} />
+      <Alert alert={alert} />
+
+      <TextForm showAlert={showAlert} heading="Address" mode={mode} />
       {/* <About /> */}
     </>
   );
